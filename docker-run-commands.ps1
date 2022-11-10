c4050d28aeb658ff1a1155a0b57b1ee29a94a35f
Bailie Wynbelt <Wynbeltb@gmail.com>
2022-11-10 14:46:52 -0700

    changed file path for my computer

--- docker-run-commands.ps1
+++ docker-run-commands.ps1
@@ -1,3 +1,3 @@
 docker run -d -e DISABLE_AUTH=true -e ROOT=TRUE --rm -p 127.0.0.3:8787:8787 `
--v C:/Users/andre/Dropbox/Dev/grazeR:/home/rstudio/grazeR `
+-v C:/Users/wynbe/grazeR:/home/rstudio/grazeR `
 amantaya/rstudio:4.2.1
