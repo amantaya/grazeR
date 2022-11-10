bc6bb8025df606731019d342ffc4ead7bfc72dc1
amantaya <aantaya@email.arizona.edu>
2022-11-10 14:34:41 -0700

    removed comment causing copy/paste to fail

--- docker-run-commands.ps1
+++ docker-run-commands.ps1
@@ -1,4 +1,3 @@
 docker run -d -e DISABLE_AUTH=true -e ROOT=TRUE --rm -p 127.0.0.3:8787:8787 `
-# add local access to grazeR files
 -v C:/Users/andre/Dropbox/Dev/grazeR:/home/rstudio/grazeR `
 amantaya/rstudio:4.2.1
