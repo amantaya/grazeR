54c595630b816d50293a56761390183cd594e1a2
amantaya <aantaya@email.arizona.edu>
2022-12-12 13:21:10 -0700

    Create test-split_ancestry_column.R

---
+++ tests/testthat/test-split_ancestry_column.R
@@ -0,0 +1,5 @@
+test_that("stops if not data frame", {
+  # vector <- c(1, 2, 3)
+  # expect_error(stop(split_ancestry_column(vector())), "is.data.frame(df) is not TRUE")
+  skip("I don't know what I'm doing")
+})
