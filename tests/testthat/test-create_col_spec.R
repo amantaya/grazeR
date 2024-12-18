127b1c8723ab87168ea4756b3f712772b80e29be
amantaya <andrew.antaya@sdstate.edu>
2024-12-18 16:59:31 +0000

    refactor: improve tests

--- tests/testthat/test-create_col_spec.R
+++ tests/testthat/test-create_col_spec.R
@@ -1,33 +1,33 @@
-test_that("preliminary greenfeed data column specification is correct", {
-    # get the preliminary greenfeed data column specification
-    preliminary_greenfeed_data_column_specification <- get_prelim_col_spec()
-    # check if the preliminary greenfeed data column specification is correct
-    expect_equal(
-        preliminary_greenfeed_data_column_specification,
-        readr::read_rds(
-            testthat::test_path(
-                    "inst",
-                    "extdata",
-                    "preliminary-greenfeed-data-column-specification.Rds"
-                )
-            )
-        )
-    )
+test_that("create_prelim_col_spec returns correct column specifications", {
+  expected_col_spec <- readr::cols(
+    FeederID = readr::col_double(),
+    AnimalName = readr::col_character(),
+    RFID = readr::col_character(),
+    StartTime = readr::col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+    EndTime = readr::col_datetime(format = "%Y-%m-%d %H:%M:%S"),
+    GoodDataDuration = readr::col_time(),
+    CO2GramsPerDay = readr::col_double(),
+    CH4GramsPerDay = readr::col_double(),
+    O2GramsPerDay = readr::col_double(),
+    H2GramsPerDay = readr::col_double(),
+    H2SGramsPerDay = readr::col_double(),
+    AirflowLitersPerSec = readr::col_double(),
+    AirflowCf = readr::col_double(),
+    WindSpeedMetersPerSec = readr::col_double(),
+    WindDirDeg = readr::col_double(),
+    WindCf = readr::col_double(),
+    WasInterrupted = readr::col_logical(),
+    InterruptingTags = readr::col_character(),
+    TempPipeDegreesCelsius = readr::col_double(),
+    IsPreliminary = readr::col_logical(),
+    RunTime = readr::col_datetime(format = "%Y-%m-%d %H:%M:%S")
+  )
+
+  actual_col_spec <- create_prelim_col_spec()
+
+  expect_equal(actual_col_spec, expected_col_spec)
 })
 
-test_that("preliminary greenfeed data schema is correct", {
-    # get the preliminary greenfeed data schema
-    preliminary_greenfeed_data_schema <- get_prelim_data_schema()
-    # check if the preliminary greenfeed data schema is correct
-    expect_equal(
-        preliminary_greenfeed_data_schema,
-        readr::read_csv(
-            testthat::test_path(
-                "inst",
-                "extdata",
-                "preliminary-greenfeed-data-schema.csv"
-            ),
-            col_types = get_prelim_col_spec()
-        )
-    )
-})
+# TODO - create a test for get_prelim_col_spec
+# test_that("get_prelim_col_spec returns correct column specifications", {
+
--- tests/testthat/test-file_utilities.R
+++ tests/testthat/test-file_utilities.R
@@ -10,7 +10,7 @@ test_that("file sha256 hash is correct", {
     # check if the sha256 hash is correct
     expect_equal(
         sha256_hash,
-        "66b01763ad80316783341083c02317e7fdda4391e58cbf02f26f2a92257c5b92"
+        "d9014c4624844aa5bac314773d6b689ad467fa4e1d1a50a1b8a99d5a95f72ff5"
     )
 })
 
@@ -26,31 +26,76 @@ test_that("file md5 hash is correct", {
     # check if the sha256 hash is correct
     expect_equal(
         md5_hash,
-        "78016cea74c298162366b9f86bfc3b16"
+        "746308829575e17c3331bbcb00c0898b"
     )
 })
 
-
-test_that("strict_read_csv correctly parses data", {
-    expected_col_spec <- get_prelim_col_spec()
-    # get the preliminary greenfeed data schema
-    parsed_data <- strict_read_csv(
+test_that("md5 hash of CSV file is correct", {
+    # use the inst/extdata/preliminary-greenfeed-data-schema.csv
+    # file to test the md5 hash function
+    actual_md5hash <- generate_md5_hash(
         testthat::test_path(
+            "..",
+            "..",
             "inst",
-            "data",
-            "preliminary-greenfeed-data.csv"
-        ) # TODO - create a test data file
-    )
-    # Define the expected data frame
-    expected_data <- tibble::tibble(
-        column1 = c("value1", "value2"),
-        column2 = c(1.0, 2.0),
-        column3 = as.Date(c("2021-01-01", "2021-01-02"))
-    ) # TODO - finish defining the expected data frame
-
-    # check if the preliminary greenfeed data schema is correct
-    expect_equal(
-        parsed_data,
-        expected_data
+            "extdata",
+            "preliminary-greenfeed-data-schema.csv"
         )
+    )
+    actual_md5hash <- as.character(actual_md5hash)
+
+    expected_md5hash <- "4710cdc12cd6f2fc62e2567dc27fe392"
+
+    # check if the md5 hash is correct
+    expect_equal(
+        actual_md5hash,
+        expected_md5hash
+    )
 })
+
+test_that("sha256 hash of CSV file is correct", {
+    # use the inst/extdata/preliminary-greenfeed-data-schema.csv
+    # file to test the md5 hash function
+    actual_sha256hash <- generate_sha256_hash(
+        testthat::test_path(
+            "..",
+            "..",
+            "inst",
+            "extdata",
+            "preliminary-greenfeed-data-schema.csv"
+        )
+    )
+    actual_sha256hash <- as.character(actual_sha256hash)
+
+    expected_sha256hash <- "454f3eea52d6a6e935825f2e0a6614825b02d454c6038e29206d6845dbf44c36"
+
+    # check if the md5 hash is correct
+    expect_equal(
+        actual_sha256hash,
+        expected_sha256hash
+    )
+})
+
+# test_that("strict_read_csv correctly parses data", {
+#     expected_col_spec <- get_prelim_col_spec()
+#     # get the preliminary greenfeed data schema
+#     parsed_data <- strict_read_csv(
+#         testthat::test_path(
+#             "inst",
+#             "data",
+#             "preliminary-greenfeed-data.csv"
+#         ) # TODO - create a test data file
+#     )
+#     # Define the expected data frame
+#     expected_data <- tibble::tibble(
+#         column1 = c("value1", "value2"),
+#         column2 = c(1.0, 2.0),
+#         column3 = as.Date(c("2021-01-01", "2021-01-02"))
+#     ) # TODO - finish defining the expected data frame
+
+#     # check if the preliminary greenfeed data schema is correct
+#     expect_equal(
+#         parsed_data,
+#         expected_data
+#         )
+# })
