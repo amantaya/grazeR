f92f64a8c0d1b1d363cd7945b04fda487cbd20cc
amantaya <andrew.antaya@sdstate.edu>
2025-02-12 22:26:55 +0000

    test: add tests for validate_eid_prefix function to verify prefix correctness

--- tests/testthat/test-data_validation.R
+++ tests/testthat/test-data_validation.R
@@ -9,3 +9,11 @@ test_that("eid <15 char in length returns FALSE", {
 test_that("eid ==15 char in length returns TRUE", {
   expect_equal(validate_eid_length("123456789012345"), TRUE)
 })
+
+test_that("eid with incorrect prefix returns FALSE", {
+  expect_equal(validate_eid_prefix("123456789012345"), FALSE)
+})
+
+test_that("eid with correct prefix returns TRUE", {
+  expect_equal(validate_eid_prefix("982012345678901"), TRUE)
+})
