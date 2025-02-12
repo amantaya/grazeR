500072d1c9f074b957604603886b7acfcfbd3d58
amantaya <andrew.antaya@sdstate.edu>
2025-02-12 22:25:45 +0000

    feat: add validate_eid_prefix function to check EID prefix

--- R/data_validation.R
+++ R/data_validation.R
@@ -14,3 +14,20 @@ validate_eid_length <- function(eid) {
         return(TRUE)
     }
 }
+
+#' @title Validate EID Prefix
+#'
+#' @param eid A character string representing the electronic identification number (EID) of an animal.
+#'
+#' @return A logical value indicating whether the EID has the correct prefix.
+#'
+#' @examples
+#'
+#' @export
+validate_eid_prefix <- function(eid) {
+    first_four_digits <- substr(eid, 1, 4)
+    if (first_four_digits != "9820") {
+        return(FALSE)
+    }
+    return(TRUE)
+}
